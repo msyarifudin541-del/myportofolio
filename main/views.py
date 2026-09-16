6a9a434672bdeefc68861804f834badc2db4bcd0
Muhammad Syarifudin <m.syarifudin541@gmail.com>
2026-09-16 13:07:32 +0700

    Tutorial 3 : menambahkan fungsi create_project pada main/views.py

--- main/views.py
+++ main/views.py
@@ -1,25 +1,47 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from main.forms import ProjectForm
 from main.models import Experience, Project
 
+
 def show_main(request):
-    context = {
-        "name": "Muhammad Syarifudin",
-        "npm": "2506657112",
-        "study_program": "S1 Ilmu Komputer",
-        "bio": "Passionate Computer Science student exploring full-stack web development and AI.",
-    }
-    return render(request, "index.html", context)
+  context = {
+      "name": "Muhammad Syarifudin",
+      "npm": "2506657112",
+      "study_program": "S1 Ilmu Komputer",
+      "bio": (
+          "Passionate Computer Science student exploring full-stack web"
+          " development and AI."
+      ),
+  }
+  return render(request, "index.html", context)
+
 
 def show_experience(request):
-    context = {
-        "name": "Muhammad Syarifudin",
-        "experience_list": Experience.objects.all(),
-    }
-    return render(request, "experience.html", context)
+  context = {
+      "name": "Muhammad Syarifudin",
+      "experience_list": Experience.objects.all(),
+  }
+  return render(request, "experience.html", context)
+
 
 def show_project(request):
-    context = {
-        "name": "Muhammad Syarifudin",
-        "project_list": Project.objects.all(),
-    }
-    return render(request, "project.html", context)
+  context = {
+      "name": "Muhammad Syarifudin",
+      "project_list": Project.objects.all(),
+  }
+  return render(request, "project.html", context)
+
+
+def create_project(request):
+  form = ProjectForm(request.POST or None)
+  if request.method == "POST" and form.is_valid():
+    form.save()
+    messages.success(request, "Proyek baru berhasil ditambahkan!")
+    return redirect("main:show_project")
+
+  context = {
+      "name": "Muhammad Syarifudin",
+      "form": form,
+  }
+  return render(request, "projects_form.html", context)
