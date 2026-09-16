c896235437f4d6b647bce129a1a1d37b2f93ee4d
Muhammad Syarifudin <m.syarifudin541@gmail.com>
2026-09-16 13:51:59 +0700

    Tutorial 3 : menambahkan path delete_project dan api/projects/ pada main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from main.views import (
     create_project,
+    delete_project,
+    get_projects_json,
     show_experience,
     show_main,
     show_project,
@@ -13,4 +15,6 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path("projects/", show_project, name="show_project"),
     path("projects/add/", create_project, name="create_project"),
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
 ]
